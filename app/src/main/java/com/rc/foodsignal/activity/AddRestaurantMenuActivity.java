b453621dd4ae7992d3040537fcda8175f1a84d56
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2018-01-31 14:51:55 +0600

    cg-fs: fully customized flowlayout

--- app/src/main/java/com/rc/foodsignal/activity/AddRestaurantMenuActivity.java
+++ app/src/main/java/com/rc/foodsignal/activity/AddRestaurantMenuActivity.java
@@ -1,5 +1,7 @@
 package com.rc.foodsignal.activity;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,15 +9,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.signature.ObjectKey;
+import com.nex3z.flowlayout.FlowLayout;
+import com.nex3z.flowlayout.FlowLayoutManager;
 import com.rc.foodsignal.R;
+import com.rc.foodsignal.model.DataFoodCategory;
+import com.rc.foodsignal.model.FoodCategory;
+import com.rc.foodsignal.model.ResponseFoodCategory;
 import com.rc.foodsignal.model.RestaurantLoginData;
+import com.rc.foodsignal.util.AllUrls;
 import com.rc.foodsignal.util.AppUtils;
+import com.rc.foodsignal.util.HttpRequestManager;
+import com.reversecoder.library.network.NetworkManager;
 import com.reversecoder.library.storage.SessionManager;
+import com.reversecoder.library.util.AllSettingsManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.rc.foodsignal.util.AllConstants.DEFAULT_FOOD_CATEGORY;
+import static com.rc.foodsignal.util.AllConstants.SESSION_FOOD_CATEGORY;
 import static com.rc.foodsignal.util.AllConstants.SESSION_RESTAURANT_LOGIN_DATA;
 
 /**
@@ -31,6 +49,13 @@ public class AddRestaurantMenuActivity extends AppCompatActivity {
     RestaurantLoginData restaurantLoginData;
 
     ImageView ivRestaurantMenu;
+
+    // Flow Layout
+    GetAllFoodCategory getAllFoodCategory;
+    ArrayList<FoodCategory> mCategory = new ArrayList<>();
+    List<String> mCategoryKey = new ArrayList<>();
+    FlowLayout flowLayout;
+    FlowLayoutManager flowLayoutManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,6 +87,18 @@ public class AddRestaurantMenuActivity extends AppCompatActivity {
 //                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
                 .apply(new RequestOptions().circleCropTransform())
                 .into(ivRestaurantMenu);
+
+        flowLayout = (FlowLayout) findViewById(R.id.fl_food_category);
+
+        if (!NetworkManager.isConnected(AddRestaurantMenuActivity.this)) {
+            Toast.makeText(AddRestaurantMenuActivity.this, getString(R.string.toast_network_error), Toast.LENGTH_SHORT).show();
+
+            setDefaultFoodCategory();
+
+        } else {
+            getAllFoodCategory = new GetAllFoodCategory(AddRestaurantMenuActivity.this);
+            getAllFoodCategory.execute();
+        }
     }
 
     private void initActions() {
@@ -78,5 +115,88 @@ public class AddRestaurantMenuActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**********************
+     * Methods for filter *
+     **********************/
+    private void initFilter(ArrayList<FoodCategory> foodCategories) {
+        mCategory = foodCategories;
+        mCategoryKey = getUniqueCategoryKeys(foodCategories);
+
+        if (mCategoryKey.size() > 0) {
+            flowLayoutManager = new FlowLayoutManager.FlowViewBuilder(AddRestaurantMenuActivity.this, flowLayout, mCategoryKey, new FlowLayoutManager.onFlowViewClick() {
+                @Override
+                public void flowViewClick(TextView updatedTextView) {
+                }
+            })
+                    .setSingleChoice(true)
+                    .build();
+        }
+    }
+
+    public List<String> getUniqueCategoryKeys(ArrayList<FoodCategory> foodCategories) {
+        List<String> categories = new ArrayList<>();
+        for (FoodCategory foodCategory : foodCategories) {
+            categories.add(foodCategory.getName());
+        }
+        Collections.sort(categories);
+        return categories;
+    }
+
+    private void setDefaultFoodCategory() {
+        if (!AllSettingsManager.isNullOrEmpty(SessionManager.getStringSetting(AddRestaurantMenuActivity.this, SESSION_FOOD_CATEGORY))) {
+            initFilter(DataFoodCategory.getResponseObject(SessionManager.getStringSetting(AddRestaurantMenuActivity.this, SESSION_FOOD_CATEGORY), DataFoodCategory.class).getData());
+        } else {
+            initFilter(DataFoodCategory.getResponseObject(DEFAULT_FOOD_CATEGORY, DataFoodCategory.class).getData());
+        }
+    }
+
+    private class GetAllFoodCategory extends AsyncTask<String, String, HttpRequestManager.HttpResponse> {
+
+        private Context mContext;
+
+        public GetAllFoodCategory(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected HttpRequestManager.HttpResponse doInBackground(String... params) {
+            HttpRequestManager.HttpResponse response = HttpRequestManager.doGetRequest(AllUrls.getAllFoodCategoryUrl());
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(HttpRequestManager.HttpResponse result) {
+
+            if (result.isSuccess() && !AppUtils.isNullOrEmpty(result.getResult().toString())) {
+                Log.d(TAG, "success response from web: " + result.getResult().toString());
+
+                ResponseFoodCategory responseFoodCategory = ResponseFoodCategory.getResponseObject(result.getResult().toString(), ResponseFoodCategory.class);
+
+                if (responseFoodCategory.getStatus().equalsIgnoreCase("1") && (responseFoodCategory.getData().size() > 0)) {
+                    Log.d(TAG, "success response from web: " + responseFoodCategory.toString());
+
+                    //Save food category into session
+                    DataFoodCategory dataFoodCategory = new DataFoodCategory(responseFoodCategory.getData());
+                    SessionManager.setStringSetting(AddRestaurantMenuActivity.this, SESSION_FOOD_CATEGORY, DataFoodCategory.getResponseString(dataFoodCategory));
+
+                    initFilter(responseFoodCategory.getData());
+
+                } else {
+
+                    setDefaultFoodCategory();
+//                    Toast.makeText(getActivity(), getResources().getString(R.string.toast_no_info_found), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+
+                setDefaultFoodCategory();
+//                Toast.makeText(getActivity(), getResources().getString(R.string.toast_could_not_retrieve_info), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ flowlayout/src/main/java/com/nex3z/flowlayout/FlowLayoutManager.java
@@ -0,0 +1,180 @@
+package com.nex3z.flowlayout;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowLayoutManager {
+
+    public enum TEXT_TYPE {SELECTED, UNSELECTED}
+
+    public interface onFlowViewClick {
+        public void flowViewClick(TextView updatedTextView);
+    }
+
+    private Context context;
+    private FlowLayout flowLayout;
+    private List<String> keys;
+    private List<TextView> flowViews = new ArrayList<>();
+    private onFlowViewClick onFlowViewClick;
+    private boolean isSingleChoice = false;
+
+    private FlowLayoutManager(Context context, FlowLayout flowLayout, List<String> keys, onFlowViewClick onFlowViewClick, boolean isSingleChoice) {
+        this.context = context;
+        this.flowLayout = flowLayout;
+        this.keys = keys;
+        this.onFlowViewClick = onFlowViewClick;
+        this.isSingleChoice = isSingleChoice;
+    }
+
+    // Helper methods
+    private TextView buildLabel(String text, final onFlowViewClick onFlowViewClick) {
+
+        final TextView textView = new TextView(context);
+        textView.setText(text);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        textView.setPadding((int) dpToPx(16), (int) dpToPx(8), (int) dpToPx(16), (int) dpToPx(8));
+        textView.setBackgroundResource(R.drawable.chip_unselected);
+        textView.setTag(TEXT_TYPE.UNSELECTED.name());
+        textView.setTextColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView updatedTextView;
+                if (textView.getTag().toString().equalsIgnoreCase(TEXT_TYPE.SELECTED.name())) {
+                    updatedTextView = unSelectFlowView(textView);
+                } else {
+                    updatedTextView = selectFlowView(textView);
+                }
+                onFlowViewClick.flowViewClick(updatedTextView);
+
+                //For single choice we need to clear all selection before update
+                if (isSingleChoice) {
+                    clearAllSelectionExceptCurrent(updatedTextView.getText().toString());
+                }
+
+                //Update list for both single choice and multi choice
+                updateTextView(updatedTextView);
+            }
+        });
+
+        return textView;
+    }
+
+    private FlowLayoutManager buildFlowView() {
+        List<TextView> mFlowContents = new ArrayList<TextView>();
+        for (String text : keys) {
+            TextView textView = buildLabel(text, onFlowViewClick);
+            flowLayout.addView(textView);
+            mFlowContents.add(textView);
+        }
+        flowViews = mFlowContents;
+        return this;
+    }
+
+    private float dpToPx(float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    private TextView selectFlowView(TextView textView) {
+        textView.setTag(TEXT_TYPE.SELECTED.name());
+        textView.setBackgroundResource(R.drawable.chip_selected);
+        textView.setTextColor(ContextCompat.getColor(context, android.R.color.white));
+        return textView;
+    }
+
+    private TextView unSelectFlowView(TextView textView) {
+        textView.setTag(TEXT_TYPE.UNSELECTED.name());
+        textView.setBackgroundResource(R.drawable.chip_unselected);
+        textView.setTextColor(ContextCompat.getColor(context, R.color.filters_header));
+        return textView;
+    }
+
+    private TextView updateTextView(TextView textView) {
+        TextView updatedTextView = null;
+        if (flowViews != null && flowViews.size() > 0) {
+            int position = getFlowViewPosition(textView.getText().toString());
+            flowViews.remove(position);
+            flowViews.add(position, textView);
+            updatedTextView = flowViews.get(position);
+        }
+        return updatedTextView;
+    }
+
+    private int getFlowViewPosition(String value) {
+        if (flowViews != null && flowViews.size() > 0) {
+            for (int i = 0; i < flowViews.size(); i++) {
+                if (flowViews.get(i).getText().toString().equalsIgnoreCase(value)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public List<TextView> getAllFlowViews() {
+        return flowViews;
+    }
+
+    public List<TextView> getSelectedFlowViews() {
+        final List<TextView> data = new ArrayList<>();
+        if (flowViews != null && flowViews.size() > 0) {
+            for (TextView textView : flowViews) {
+                if (textView.getTag().toString().equalsIgnoreCase(TEXT_TYPE.SELECTED.name())) {
+                    data.add(textView);
+                }
+            }
+        }
+        return data;
+    }
+
+    private List<TextView> clearAllSelectionExceptCurrent(String value) {
+        final List<TextView> data = new ArrayList<>();
+        if (flowViews != null && flowViews.size() > 0) {
+            for (int i = 0; i < flowViews.size(); i++) {
+                TextView updatedTextView;
+                if (!flowViews.get(i).getText().toString().equalsIgnoreCase(value)) {
+                    updatedTextView = unSelectFlowView(flowViews.get(i));
+                } else {
+                    updatedTextView = flowViews.get(i);
+                }
+                data.add(updatedTextView);
+            }
+        }
+        return data;
+    }
+
+    public static class FlowViewBuilder {
+
+        private Context mContext;
+        private FlowLayout mFlowLayout;
+        private List<String> mKeys;
+        private boolean mIsSingleChoice = false;
+        private onFlowViewClick mOnFlowViewClick;
+
+        public FlowViewBuilder(Context context, FlowLayout flowLayout, List<String> keys, onFlowViewClick onFlowViewClick) {
+            mContext = context;
+            mFlowLayout = flowLayout;
+            mKeys = keys;
+            mOnFlowViewClick = onFlowViewClick;
+        }
+
+        public FlowViewBuilder setSingleChoice(boolean isSingleChoice) {
+            mIsSingleChoice = isSingleChoice;
+            return this;
+        }
+
+        public FlowLayoutManager build() {
+            FlowLayoutManager flowLayoutManager = new FlowLayoutManager(mContext, mFlowLayout, mKeys, mOnFlowViewClick, mIsSingleChoice)
+                    .buildFlowView();
+            return flowLayoutManager;
+        }
+    }
+}
